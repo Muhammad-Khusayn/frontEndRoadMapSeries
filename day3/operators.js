fa0541990e5f7ff90b5eeb92cd39dd2fc94da7aa
Muhammad <isayevm17@gmail.com>
2023-12-12 23:53:37 +0900

    notes from yesterday

---
+++ day3/operators.js
@@ -0,0 +1,62 @@
+// Strings 2 ways of declaring strings:
+
+const myString1 = "Hello World";
+
+const myString2 = "Hello World";
+
+// Numbers
+
+const myNumber = 20;
+
+// Boolean
+
+const myBoolean = true;
+
+// Arrays
+
+const myArray = ["messi", "is", "the", "best"];
+
+// Objects
+
+const myObject = {
+  property1: "Messi",
+  property2: "Suarez",
+  property3: "Neymar",
+};
+// Access the object properties:
+
+myObject.property1;
+
+myObject["property1"];
+
+/* Operator types
+
+1. Arithmetic:
+
++ - * / % ** ++ --
+
+% is a modulus sign which find remainder after devision
+
+++, -- increment and decrement signs
+
+Remember you cannot use incrementation and decrementation with cons vaiable instead us let.
+
+2. Assigment operators
+
+= += -= *= /=
+You dont really need other assignment operators then = even if can use them tray not to. Write longer form of the code
+
+EX:
+
+let messi = 10
+
+messi += 5
+
+15
+
+3. Comparison operators
+
+== === != !== < > <= >=
+
+
+*/
