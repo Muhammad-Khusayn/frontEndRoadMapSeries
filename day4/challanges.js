993dfea3c8db589fb7f968e084d6f6da923511be
Muhammad <isayevm17@gmail.com>
2023-12-17 00:18:27 +0900

    Finished all the problems on the codeWars here are some of them

--- day4/challanges.js
+++ day4/challanges.js
@@ -9,4 +9,52 @@ function getSum(sm) {
   return sum;
 }
 
-// console.log(getSum([2, 2, 2, 2])
+// console.log(getSum([2, 2, 2, 2]));
+
+// switch (win) {
+//   case ("rock", "scissors"):
+//     console.log("Player 1 won!");
+//     break;
+//   case ("scissors", "paper"):
+//     console.log("Player 1 won!");
+//     break;
+//   case ("paper", "rock"):
+//     console.log("Player 1 won!!");
+//     break;
+//   case ("scissors", "rock"):
+//     console.log("Player 2 won");
+//     break;
+//   case ("paper", "scissors"):
+//     console.log("Player 2 won");
+//     break;
+//   case ("rock", "paper"):
+//     console.log("Player 2 won");
+//     break;
+//   case ("rock", "rock"):
+//     console.log("Draw!");
+//     break;
+//   case ("scissors", "scissors"):
+//     console.log("Draw!");
+//     break;
+//   case ("paper", "paper"):
+//     console.log("Draw!");
+//     break;
+//   default:
+//     console.log("no color was found");
+// }
+
+const rps = (p1, p2) => {
+  if (p1 == p2) {
+    return "Draw!";
+  }
+
+  if (p1 == "scissors" && p2 == "paper") {
+    return "Player 1 won!";
+  } else if (p1 == "rock" && p2 == "scissors") {
+    return "Player 1 won!";
+  } else if (p1 == "paper" && p2 == "rock") {
+    return "Player 1 won!";
+  } else {
+    return "Player 2 won!";
+  }
+};
--- day4/switchCase.js
+++ day4/switchCase.js
@@ -23,3 +23,35 @@ switch (randomColor) {
   default:
     console.log("no color was found");
 }
+
+function noSpace(str) {
+  const arr = str.split(" ");
+  let newArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const trimmedString = arr[i].trim();
+    newArray.push(trimmedString);
+  }
+
+  const newString = newArray.join("");
+
+  return newString;
+}
+
+console.log(noSpace("messi is the best in the world"));
+
+function maps(x) {
+  let outputArray = Array.from(x, (x) => x * 2);
+  return outputArray;
+}
+
+console.log(maps([1, 2, 3, 4]));
+
+var summation = function (num) {
+  let result = 0;
+  for (var i = 1; i <= num; i++) {
+    result += i;
+  }
+
+  return result;
+};
