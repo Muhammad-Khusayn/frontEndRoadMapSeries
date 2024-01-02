d6f541a30db066b31a413625a62481d3e7d862ae
Muhammad <isayevm17@gmail.com>
2024-01-02 21:06:03 +0900

    string methods

--- day7/stringMethods.js
+++ day7/stringMethods.js
@@ -1,2 +1,6 @@
 // ================ Methods =============
+const str = "Messi is the best player in the world";
+
+console.log(str.toUpperCase());
+
 // replaceAll. toUpperCase, substring, trim, match
